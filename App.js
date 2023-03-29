3d4185fe02d0525b15d636aa72b969f0d6b4e50c
vibin17604 <108916451+vibin17604@users.noreply.github.com>
2023-03-29 11:11:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Smile from './Daily challenge/Day 2/Smile';
+
+
+function App() {
+  return (
+    <div >
+      <center>
+    <Smile/>
+    </center>
+    </div>
+  );
+}
+
+export default App;
---
+++ Smile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Smile() {
+  return (
+    <div>
+      <h1>Smile component</h1>
+      <p>its a functional component</p>
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/002/592/172/small/smile-emoji-pop-art-line-style-icon-free-vector.jpg"alt="smile"></img>
+    </div>
+  )
+}
+
+export default Smile
